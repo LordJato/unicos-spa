4aeed41ee63060d60ad99a63e586a44253647310
Steto Javellana <javellanasteto99@gmail.com>
2024-10-06 22:42:24 +0800

    add pinia and vue router

--- src/plugins/index.js
+++ src/plugins/index.js
@@ -6,7 +6,14 @@
 
 // Plugins
 import vuetify from './vuetify'
+import { createPinia } from 'pinia';
 
 export function registerPlugins (app) {
+  const pinia = createPinia()
+  pinia.use(({ store }) => {
+      store.router = markRaw(router)
+  })
+  
   app.use(vuetify)
+  app.use(pinia)
 }
