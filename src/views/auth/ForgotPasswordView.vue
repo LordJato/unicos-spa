bd52ea1bf019073bb1865b666d94a888bf2979b7
Steto Javellana <javellanasteto99@gmail.com>
2024-10-26 23:22:45 +0800

    preparing reset password

--- src/views/auth/ForgotPasswordView.vue
+++ src/views/auth/ForgotPasswordView.vue
@@ -8,26 +8,19 @@
               <VImg :src="unicosLogo" max-height="60" class="my-5" />
               <VCard class="py-5 px-9 rounded-xl">
                 <VForm @submit.prevent="register" class="text-center">
-                <h1 class="text-center font-weight-black my-2">Forgot Password?</h1>
-                <span class="text-caption ">Remember your password? </span>
-                <RouterLink class="text-caption text-decoration-none text-primary" to="/login">
-                  Login here</RouterLink>
-                 <VTextField
-                  v-model="form.name"
-                  class="mt-6 mb-1 nb"
-                  label="Email"
-                  type="email"
-                  prepend-inner-icon="mdi-email"
-                  variant="outlined"
-                  color="primary"
-                  density="compact"
-                />
-                <v-btn type="submit" block color="primary" size="large" rounded="">
-                  Reset Password
-                </v-btn>
-              </VForm>
+                  <h1 class="text-center font-weight-black my-2">Forgot Password?</h1>
+                  <span class="text-caption ">Remember your password? </span>
+                  <RouterLink class="text-caption text-decoration-none text-primary" to="/login">
+                    Login here</RouterLink>
+                  <VTextField v-model="form.name" class="mt-6 mb-1 nb" label="Email" type="email"
+                    prepend-inner-icon="mdi-email" variant="outlined" color="primary" density="compact" />
+                  <v-btn type="submit" block color="primary" size="large" rounded="">
+                    Reset Password
+                  </v-btn>
+                </VForm>
               </VCard>
-             
+              <p class="text-caption text-center mt-2">Contact Us | support@unicos.com </p>
+
             </VCol>
           </VRow>
         </VCol>
@@ -35,38 +28,38 @@
     </VContainer>
   </section>
 
-  </template>
-  
-  <script setup>
-  import { reactive } from 'vue'
-  import { useUserStore } from '@/stores/user'
-  import unicosLogo from "@/assets/img/logo.png"
-  
-  const userStore = useUserStore()
-  
-  const initialForm = {
-    name: "",
-    email: "",
-    password: "",
-    password_confirmation: "",
-  };
-  
-  const form = reactive({
-    ...initialForm,
-  });
-  
-  const register = async () => {
-    try {
-      const register = await userStore.registerUser(form)
-      console.log('Registration successful!', register);
-    } catch (error) {
-      console.error('Registration failed:', error);
-    }
+</template>
+
+<script setup>
+import { reactive } from 'vue'
+import { useUserStore } from '@/stores/user'
+import unicosLogo from "@/assets/img/logo.png"
+
+const userStore = useUserStore()
+
+const initialForm = {
+  name: "",
+  email: "",
+  password: "",
+  password_confirmation: "",
+};
+
+const form = reactive({
+  ...initialForm,
+});
+
+const register = async () => {
+  try {
+    const register = await userStore.registerUser(form)
+    console.log('Registration successful!', register);
+  } catch (error) {
+    console.error('Registration failed:', error);
   }
-  
-  </script>
-  
-  <style scoped>
+}
+
+</script>
+
+<style scoped>
 #forgotPasswordSection {
   position: relative;
   display: flex;
@@ -75,4 +68,4 @@
   min-height: 100vh;
   background: #e9f2fb;
 }
-  </style>
+</style>
--- src/views/auth/ResetPasswordView.vue
+++ src/views/auth/ResetPasswordView.vue
@@ -1,129 +1,77 @@
 <template>
+  <section id="resetPasswordSection">
     <VContainer class="fill-height pa-0" fluid>
-      <v-btn
-        position="fixed"
-        color="primary"
-        prepend-icon="$vuetify"
-        location="top right"
-        class="ma-5"
-        router-link
-        to="/login"
-        >Login</v-btn
-      >
-      <VRow class="h-100">
-        <VCol
-          cols="12"
-          md="6"
-          class="rounded-br-xl d-flex justify-center align-center"
-        >
-          <VImg :src="registerBG" max-height="400" />
-        </VCol>
-        <VCol cols="12" md="6">
+      <VRow class="h-100" justify="center" align="center">
+        <VCol cols="6">
           <VRow class="h-100 ma-0 pa-0" justify="center" align="center">
             <VCol cols="12" md="8">
-              <VForm @submit.prevent="register">
-                <VImg
-                  :src="registerAvatar"
-                  max-height="150"
-                />
-                <h1 class="text-center font-weight-black my-2">RESET PASSWORD</h1>
-                 <VTextField
-                  v-model="form.name"
-                  class="my-4"
-                  label="Name"
-                  type="email"
-                  prepend-inner-icon="mdi-account"
-                  variant="outlined"
-                  color="primary"
-                  density="compact"
-                />
-                <VTextField
-                  v-model="form.email"
-                  class="my-4"
-                  label="Email"
-                  type="email"
-                  prepend-inner-icon="mdi-email"
-                  variant="outlined"
-                  color="primary"
-                  density="compact"
-                />
-  
-                <VTextField
-                  v-model="form.password"
-                  class="mt-4"
-                  label="Password"
-                  type="password"
-                  prepend-inner-icon="mdi-lock"
-                  variant="outlined"
-                  color="primary"
-                  density="compact"
-                />
-                   <VTextField
-                  v-model="form.password_confirmation"
-                  class="mt-4"
-                  label="Confirm Password"
-                  type="password"
-                  prepend-inner-icon="mdi-lock"
-                  variant="outlined"
-                  color="primary"
-                  density="compact"
-                />
-                <v-btn type="submit" block class="mb-8" color="primary" size="large" rounded="">
-                  Register
-                </v-btn>
-  
-                <!-- <div
-                  class="d-flex justify-space-between align-center mx-10 mb-16"
-                >
-                  <v-btn icon="mdi-google" color="red"> </v-btn>
-                  <v-btn icon="mdi-facebook" color="blue"> </v-btn>
-                  <v-btn icon="mdi-twitter" color="light-blue lighten-3"> </v-btn>
-                </div> -->
-              </VForm>
+              <VImg :src="unicosLogo" max-height="60" class="my-5" />
+              <VCard class="py-5 px-9 rounded-xl">
+                <VForm @submit.prevent="register" class="text-center">
+                  <h1 class="text-center font-weight-black my-2">Reset Password</h1>
+                  <span class="text-caption ">Remember your password? </span>
+                  <RouterLink class="text-caption text-decoration-none text-primary" to="/login">
+                    Login here</RouterLink>
+                  <VTextField v-model="form.name" class="mt-6 mb-1 nb" label="Email" type="email"
+                    prepend-inner-icon="mdi-email" variant="outlined" color="primary" density="compact" />
+
+                  <div class="d-flex justify-space-between">
+                    <RouterLink class="text-caption text-decoration-none text-primary" to="/login">
+                      Back</RouterLink>
+                    <v-btn type="submit" color="primary" size="large" rounded="">
+                      Confirm Reset
+                    </v-btn>
+                  </div>
+
+                </VForm>
+              </VCard>
+              <p class="text-caption text-center mt-2">Contact Us | support@unicos.com </p>
+
             </VCol>
           </VRow>
         </VCol>
       </VRow>
     </VContainer>
-  </template>
-  
-  <script setup>
-  import { reactive } from 'vue'
-  import { useUserStore } from '@/stores/user'
-  import registerBG from '@/assets/img/register-bg.svg'
-  import registerAvatar from '@/assets/img/register-avatar.svg'
-  
-  const userStore = useUserStore()
-  
-  const initialForm = {
-    name: "",
-    email: "",
-    password: "",
-    password_confirmation: "",
-  };
-  
-  const form = reactive({
-    ...initialForm,
-  });
-  
-  const register = async () => {
-    try {
-      const register = await userStore.registerUser(form)
-      console.log('Registration successful!', register);
-    } catch (error) {
-      console.error('Registration failed:', error);
-    }
+  </section>
+
+</template>
+
+<script setup>
+import { reactive } from 'vue'
+import { useUserStore } from '@/stores/user'
+import unicosLogo from "@/assets/img/logo.png"
+
+const userStore = useUserStore()
+
+const initialForm = {
+  name: "",
+  email: "",
+  password: "",
+  password_confirmation: "",
+};
+
+const form = reactive({
+  ...initialForm,
+});
+
+const register = async () => {
+  try {
+    const register = await userStore.registerUser(form)
+    console.log('Registration successful!', register);
+  } catch (error) {
+    console.error('Registration failed:', error);
   }
-  
-  </script>
-  
-  <style scoped>
-  .rounded-bl-xl {
-    background-color: #007acc;
-    border-bottom-left-radius: 750px !important;
-  }
-  .rounded-br-xl {
-    background-color: #007acc;
-    border-bottom-right-radius: 750px !important;
-  }
-  </style>
+}
+
+</script>
+
+<style scoped>
+#resetPasswordSection {
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+  background: #e9f2fb;
+}
+</style>
