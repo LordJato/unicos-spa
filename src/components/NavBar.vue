9cf1751c84c1667efe24e1cbb485d82261812ea8
Steto Javellana <javellanasteto99@gmail.com>
2024-12-04 17:54:42 +0800

    fix navigation

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,213 +1,122 @@
 <template>
-    <VNavigationDrawer
-      v-model="drawer"
-      :rail="rail"
-      permanent
-      @click="rail = false"
-    >
-      <v-list>
-        <v-list-item
-          :prepend-avatar="defaultAvatar"
-          title="Ye Futian"
-        >
-          <template v-slot:append>
-            <v-btn
-              variant="text"
-              icon="mdi-chevron-left"
-              @click.stop="
-                rail = !rail;
-                open = null;
-              "
-            ></v-btn>
-          </template>
-        </v-list-item>
-      </v-list>
-  
-      <v-divider></v-divider>
-      <v-list v-model:opened="open" nav>
-        <!-- DASHBOARD -->
-        <v-list-item
-          title="Dashboard"
-          prepend-icon="mdi-view-dashboard"
-          value="Dashboard"
-            @click="$router.push({name : 'dashboard'})"
-        >
-          <v-tooltip v-if="rail" activator="parent" location="bottom"
-            >Dashboard</v-tooltip
-          ></v-list-item
-        >
-  
-        <!-- RECRUITMENT -->
-        <v-list-group value="Recruitment">
-          <template v-slot:activator="{ props }">
-            <v-list-item
-              v-bind="props"
-              title="Recruitment"
-              prepend-icon="mdi-account-search"
-            ></v-list-item>
-            <v-tooltip v-if="rail" activator="parent" location="bottom"
-              >Recruitment</v-tooltip
-            >
-          </template>
-  
-          <v-list-item
-            v-for="([title, icon], i) in recruitment"
-            :key="i"
-            :title="title"
-            :prepend-icon="icon"
-            :value="title"
-          ></v-list-item>
-        </v-list-group>
-  
-        <!-- HRIS -->
-        <v-list-group value="Hris">
-          <template v-slot:activator="{ props }">
-            <v-list-item
-              v-bind="props"
-              title="HRIS"
-              prepend-icon="mdi-folder-account"
-            ></v-list-item>
-            <v-tooltip v-if="rail" activator="parent" location="bottom"
-              >HRIS</v-tooltip
-            >
-          </template>
-  
-          <v-list-item
-            v-for="([title, icon], i) in hris"
-            :key="i"
-            :title="title"
-            :prepend-icon="icon"
-            :value="title"
-          ></v-list-item>
-        </v-list-group>
-  
-        <!-- PAYROLL -->
-        <v-list-group value="Payroll">
-          <template v-slot:activator="{ props }">
-            <v-list-item
-              v-bind="props"
-              title="Payroll"
-              prepend-icon="mdi-notebook"
-            ></v-list-item>
-            <v-tooltip v-if="rail" activator="parent" location="bottom"
-              >Payroll</v-tooltip
-            >
-          </template>
-  
-          <v-list-item
-            v-for="([title, icon], i) in payroll"
-            :key="i"
-            :title="title"
-            :prepend-icon="icon"
-            :value="title"
-          ></v-list-item>
-        </v-list-group>
-  
-        <!-- CALENDAR -->
-        <v-list-item
-          title="Calendar"
-          prepend-icon="mdi-calendar"
-          value="Calendar"
-        >
-          <v-tooltip v-if="rail" activator="parent" location="bottom"
-            >Calendar</v-tooltip
-          ></v-list-item
-        >
-  
-        <!-- ACCOUNT -->
-        <VListItem
-          title="Accounts"
-          prepend-icon="mdi-account-box-multiple"
-          value="Account"
-          @click="$router.push({name : 'accounts'})"
-        >
-          <VTooltip v-if="rail" activator="parent" location="bottom"
-            >Accounts</VTooltip
-          ></VListItem
-        >
-  
-        <!-- SETUP -->
-        <v-list-group value="Setup">
-          <template v-slot:activator="{ props }">
-            <v-list-item
-              v-bind="props"
-              title="Setup"
-              prepend-icon="mdi-cog"
-            ></v-list-item>
-            <v-tooltip v-if="rail" activator="parent" location="bottom"
-              >Setup</v-tooltip
-            >
-          </template>
-  
-          <v-list-item
-            v-for="([title, icon], i) in setup"
-            :key="i"
-            :title="title"
-            :prepend-icon="icon"
-            :value="title"
-          ></v-list-item>
-        </v-list-group>
-      </v-list>
-    </VNavigationDrawer>
-  </template>
-  
-  <script setup>
-  import { ref } from "vue";
-  import defaultAvatar from "@/assets/img/default-profile.jpg"
-  
-  const drawer = ref(true);
-  const rail = ref(true);
-  
-  const open = ref(null);
-  
-  const recruitment = [
-    ["Job Opportunities", "mdi-list-box"],
-    ["Appointment", "mdi-calendar-clock"],
-  ];
-  
-  const hris = [
-    ["Employee", "mdi-account-group"],
-    ["Shift", "mdi-calendar-month"],
-    ["Attendance", "mdi-note-check"],
-    ["Leave", "mdi-island"],
-    ["Overtime", "mdi-clock-time-eight"],
-    ["Undertime", "mdi-clock-time-four-outline"],
-    ["Holiday", "mdi-newspaper-variant"],
-    ["Events", "mdi-party-popper"],
-  ];
-  
-  const payroll = [
-    ["Generate", "mdi-compost"],
-    ["Group", "mdi-group"],
-    ["Cycle", "mdi-recycle-variant"],
-  ];
-  
-  // const account = [
-  //   ["Management", "mdi-account-multiple-outline"],
-  //   ["Settings", "mdi-cog-outline"],
-  // ];
-  
-  const setup = [
-    ["SSS", "mdi-alpha-s-box"],
-    ["Philhealth", "mdi-alpha-p-box"],
-    ["Pagibig", "mdi-hand-extended"],
-  ];
-  </script>
-  
-  <style>
-  .border {
-    margin-left: 8px;
-    margin-right: 8px;
-    background: #704232;
-    border-radius: 20px;
-    text-decoration: none;
-  }
-  .v-list-item-group .v-list-item--active {
-    color: white !important;
-  }
-  .theme--light.v-list-item--active .v-list-item__subtitle,
-  .theme--light.v-list-item .v-list-item__action-text {
-    color: white !important;
-  }
-  
-  </style>
+  <VNavigationDrawer v-model="drawer" rail expand-on-hover @mouseleave="open = null">
+    <VList class="py-3">
+      <VListItem :prepend-avatar="defaultAvatar" title="Ye Futian">
+      </VListItem>
+    </VList>
+
+    <VDivider />
+    <VList v-model:opened="open" nav>
+      <template v-for="(list, i) in lists" :key="i">
+        <VListItem v-if="list.listGroup.length === 0" :title="list.title" :value="list.title" :prepend-icon="list.icon"
+          @click="$router.push({ name: list.link })">
+        </VListItem>
+        <VListGroup v-else :value="list.title">
+        <template v-slot:activator="{ props }">
+          <VListItem v-bind="props" :title="list.title" :prepend-icon="list.icon"></VListItem>
+        </template>
+
+        <VListItem v-for="(item, itemIndex) in list.listGroup" 
+          :key="itemIndex" 
+          :title="item[0]" 
+          :prepend-icon="item[1]" 
+          :value="item[0]" 
+          @click="$router.push({ name: item[2] })"
+        />
+      </VListGroup>
+      </template>
+    </VList>
+  </VNavigationDrawer>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import defaultAvatar from "@/assets/img/default-profile.jpg"
+
+const drawer = ref(true);
+const open = ref(null);
+
+
+const lists = [
+  {
+    title: 'Dashboard',
+    listGroup: [],
+    link: 'dashboard',
+    icon: 'mdi-view-dashboard'
+  },
+  {
+    title: 'Recruitment',
+    listGroup: [
+      ["Job Opportunities", "mdi-list-box", "jobOpportunities"],
+      ["Appointment", "mdi-calendar-clock", "appointment"],
+    ],
+    link: 'recruitment',
+    icon: 'mdi-account-search'
+  },
+  {
+    title: 'HRIS',
+    listGroup: [
+      ["Employees", "mdi-account-group", "employees" ],
+      ["Shift", "mdi-calendar-month", "shift"],
+      ["Attendance", "mdi-note-check", "attendance"],
+      ["Leave", "mdi-island", "leave"],
+      ["Overtime", "mdi-clock-time-eight", "overtime"],
+      ["Undertime", "mdi-clock-time-four-outline", "undertime"],
+      ["Holiday", "mdi-newspaper-variant", "holiday"],
+      ["Events", "mdi-party-popper", "events"],
+    ],
+    link: 'hris',
+    icon: 'mdi-folder-account'
+  },
+  {
+    title: 'Payroll',
+    listGroup: [
+      ["Generate", "mdi-compost" , "payrollGenerate"],
+      ["Group", "mdi-group", "payrollGroup"],
+      ["Cycle", "mdi-recycle-variant", "payrollCycle"],
+    ],
+    icon: 'mdi-notebook'
+  },
+  {
+    title: 'Calendar',
+    listGroup: [],
+    link: 'calendar',
+    icon: 'mdi-calendar'
+  },
+  {
+    title: 'Accounts',
+    listGroup: [],
+    link: 'accounts',
+    icon: 'mdi-account-box-multiple'
+  },
+  {
+    title: 'Setup',
+    listGroup: [
+      ["SSS", "mdi-alpha-s-box" , "sss"],
+      ["Philhealth", "mdi-alpha-p-box", "philhealth"],
+      ["Pagibig", "mdi-hand-extended", "pagibig"],
+    ],
+    link: 'setup',
+    icon: 'mdi-cog'
+  },
+]
+</script>
+
+<style>
+.border {
+  margin-left: 8px;
+  margin-right: 8px;
+  background: #704232;
+  border-radius: 20px;
+  text-decoration: none;
+}
+
+.v-list-item-group .v-list-item--active {
+  color: white !important;
+}
+
+.theme--light.v-list-item--active .v-list-item__subtitle,
+.theme--light.v-list-item .v-list-item__action-text {
+  color: white !important;
+}
+</style>
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -13,28 +13,47 @@ import { createVuetify } from 'vuetify'
 
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 
-const myCustomLightTheme = {
+const lightTheme = {
   dark: false,
   colors: {
+    background: '#FFFFFF',
+    surface: '#FFFFFF',
     primary: '#213268',
-    secondary: '#5CB85C',
+    secondary: '#36c5c8',
     error: '#B00020',
     info: '#2196F3',
     success: '#4CAF50',
     warning: '#FB8C00',
-    'white-primary' : '#FAFAFA',
-    'white-secondary' : '#e9f2fb',
-    'primary-1' : '#133A85',
-    'primary-2' : '#1E2C53',
-    'primary-3' : '#161A33'
+    'white-primary': '#e9f2fb',
+    'on-surface': '#212121',
+    'on-background': '#212121',
+  }
+}
+
+const darkTheme = {
+  dark: true,
+  colors: {
+    background: '#070f2b',
+    surface: '#070F2B',
+    primary: '#213268',
+    secondary: '#36c5c8',
+    error: '#B00020',
+    info: '#2196F3',
+    success: '#4CAF50',
+    warning: '#FB8C00',
+    'white-primary': '#9DB2BF',
+    'on-surface': '#9DB2BF',
+    'on-background': '#9DB2BF',
+    'dark-grey': '#0b1420'
   },
 }
 
 export default createVuetify({
   theme: {
-    defaultTheme: 'myCustomLightTheme',
+    defaultTheme: 'darkTheme',
     themes: {
-      myCustomLightTheme,
+      lightTheme,
+      darkTheme,
     },
   },
 })
