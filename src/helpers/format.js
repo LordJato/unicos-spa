18686f2c290569de81dcac8fb2efee313c8e769a
Steto Javellana <javellanasteto99@gmail.com>
2024-12-14 23:23:02 +0800

    add helper format

---
+++ src/helpers/format.js
@@ -0,0 +1,7 @@
+export function useFormatDate(date) {
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
+    const year = date.getFullYear();
+
+    return `${month.toString().padStart(2, '0')}/${day.toString().padStart(2, '0')}/${year}`;
+}
