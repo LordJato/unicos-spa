9257243a0b08c75c25b372a41b2cadb778b60dc2
Steto Javellana <javellanasteto99@gmail.com>
2024-12-18 20:57:37 +0800

    implemented apex chart

--- src/plugins/index.js
+++ src/plugins/index.js
@@ -10,6 +10,7 @@ import router from '@/router'
 import { createPinia } from 'pinia';
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import { markRaw } from 'vue'
+import VueApexCharts from "vue3-apexcharts";
 
 export function registerPlugins(app) {
   const pinia = createPinia()
@@ -20,4 +21,5 @@ export function registerPlugins(app) {
   app.use(router)
   app.use(vuetify)
   app.use(pinia)
+  app.use(VueApexCharts);
 }
