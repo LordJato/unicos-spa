073281481e88741a20bcba961b12b1c223b0da11
Steto Javellana <javellanasteto99@gmail.com>
2024-12-19 22:14:03 +0800

    cleaning by seperate component

---
+++ src/components/Dashboard/BirthDays.vue
@@ -0,0 +1,50 @@
+<template>
+    <div>
+        <VCard elevation="6">
+            <VCardTitle class="d-flex">
+                <div>Birthday</div>
+                <VSpacer />
+                <VSelect label="This Month"
+                    :items="['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']"
+                    variant="outlined" density="compact" class="text-subtitle-1" style="font-size: 10px !important;">
+                </VSelect>
+            </VCardTitle>
+
+            <VCardText>
+                <VList>
+                    <VListItem class="pa-0" v-for="(item, i) in birthdays">
+                        <VListItemTitle class="text-body-2">{{ item.name }}</VListItemTitle>
+                        <VListItemSubtitle class="text-caption">{{ item.position }}</VListItemSubtitle>
+                        <template v-slot:prepend>
+                            <VAvatar size="30" density="compact" class="pa-0"  rounded="0">
+                                <VImg :src="item.image" :alt="item.name" />
+                            </VAvatar>
+                        </template>
+                        <template v-slot:append>
+                            <span class="text-caption"> {{ formatDate(item.birthdate) }}</span>
+                        </template>
+                    </VListItem>
+                </VList>
+            </VCardText>
+        </VCard>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+import { formatDate } from '@/helpers/format'
+import { ref } from 'vue'
+
+const birthdays = ref([
+    {
+        image: 'https://www.profilebakery.com/wp-content/uploads/2023/04/PROFILE-PICTURE-FOR-FACEBOOK.jpg',
+        name: 'Juan Miguel Reyes Santos',
+        position: 'CTO',
+        birthdate: new Date('1974-12-25'),
+    },
+    
+]);
+
+</script>
+
+<style scoped></style>
---
+++ src/components/Dashboard/WhosOnline.vue
@@ -0,0 +1,100 @@
+<template>
+    <div>
+        <VCard elevation="6">
+            <VCardTitle>Who's Online?</VCardTitle>
+            <VCardText>
+                <VDataTable v-model="selected" :items="items" item-value="name" :headers="headers" dense>
+                    <template v-slot:item.image="{ item }">
+                        <v-avatar size="30" density="compact" class="pa-0">
+                            <v-img :src="item.image" :alt="item.name" />
+                        </v-avatar>
+                    </template>
+                    <template v-slot:item.online="{ item }">
+                        <v-chip :color="getColor(item.online)">
+                            {{ item.online ? 'Online' : 'Offline' }}
+                        </v-chip>
+                    </template>
+                    <template v-slot:item.actions="{ item }">
+                        <v-icon class="me-2" size="small" @click="editItem(item)" color="secondary">
+                            mdi-pencil
+                        </v-icon>
+                        <v-icon size="small" @click="deleteItem(item)" color="error">
+                            mdi-delete
+                        </v-icon>
+                    </template>
+                    <template v-slot:no-data>
+                        <v-btn color="primary" @click="initialize">
+                            Reset
+                        </v-btn>
+                    </template>
+                </VDataTable>
+            </VCardText>
+        </VCard>
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const selected = ref([])
+const headers = [
+  { title: '', align: 'center', sortable: false, key: 'image', width: '1%', },
+  { title: 'Name', align: 'start', sortable: false, key: 'name' },
+  { title: 'Position', align: 'start', key: 'position' },
+  { title: 'Department', align: 'start', key: 'department' },
+  { title: 'Email', align: 'start', key: 'email' },
+  { title: 'Status', align: 'start', key: 'online' },
+  { title: 'Actions', align: 'start', key: 'actions' },
+]
+
+const items = [
+  {
+    image: 'https://www.profilebakery.com/wp-content/uploads/2023/04/PROFILE-PICTURE-FOR-FACEBOOK.jpg',
+    name: 'Juan Miguel Reyes Santos',
+    position: 'CTO',
+    department: 'Information Technology (IT)',
+    email: 'cto@unicos.com',
+    online: true,
+  },
+  {
+    image: 'https://www.profilebakery.com/wp-content/uploads/2023/04/women-AI-Profile-Picture.jpg',
+    name: 'Maria Sofia Rodriguez',
+    position: 'HR Head',
+    department: 'Human Resources (HR)',
+    email: 'hrhead@unicos.com',
+    online: false,
+  },
+  {
+    image: 'https://www.profilebakery.com/wp-content/uploads/2023/04/Profile-Image-AI.jpg',
+    name: 'Luis Francisco Gonzales',
+    position: 'CBDO',
+    department: 'Business Development',
+    email: 'cbdo@unicos.com',
+    online: true,
+  },
+  {
+    image: 'https://www.profilebakery.com/wp-content/uploads/2023/04/LINKEDIN-Profile-Picture-AI.jpg',
+    name: 'Gabriel Jose Velasco',
+    position: 'CMO',
+    department: 'Marketing',
+    email: 'cmo@unicos.com',
+    online: true,
+  },
+  {
+    image: 'https://www.profilebakery.com/wp-content/uploads/2023/04/AI-Profile-Picture.jpg',
+    name: 'Ana Isabella Garcia',
+    position: 'Chief Financial Officer',
+    department: 'Finance',
+    email: 'cfo@unicos.com',
+    online: false,
+  },
+]
+
+function getColor(status) {
+  return status ? 'success' : 'grey'
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
