99771350654de111c291d322a0d5e4bc80fa6e11
Steto Javellana <javellanasteto99@gmail.com>
2024-12-30 23:13:25 +0800

    add UI for logout

--- src/components/AppBar.vue
+++ src/components/AppBar.vue
@@ -18,19 +18,31 @@
       <v-icon>mdi-email</v-icon>
     </v-btn>
     <template v-slot:append>
-      <v-list>
-          <v-list-item
-            prepend-avatar="https://imgix.ranker.com/user_node_img/50082/1001639646/original/being-strong-photo-u1?auto=format&q=60&fit=crop&fm=pjpg&dpr=2&w=355"
-            title="Steto Javellana"
-          >
-            <template v-slot:append>
-              <v-btn
-                icon="mdi-menu-down"
-                variant="text"
-              ></v-btn>
-            </template>
-          </v-list-item>
-        </v-list>
+      <v-list density="compact">
+        <v-list-item
+          prepend-avatar="https://imgix.ranker.com/user_node_img/50082/1001639646/original/being-strong-photo-u1?auto=format&q=60&fit=crop&fm=pjpg&dpr=2&w=355"
+          title="Steto Javellana">
+          <template v-slot:append>
+            <v-menu>
+              <template v-slot:activator="{ props }">
+                <v-btn icon="mdi-dots-vertical" variant="text" v-bind="props"></v-btn>
+              </template>
+
+
+
+              <v-list>
+                <v-list-item v-for="(item, i) in items" :key="i" :value="item">
+                  <template v-slot:prepend>
+                    <v-icon :icon="item.icon"></v-icon>
+                  </template>
+
+                  <v-list-item-title v-text="item.text"></v-list-item-title>
+                </v-list-item>
+              </v-list>
+            </v-menu>
+          </template>
+        </v-list-item>
+      </v-list>
     </template>
   </VAppBar>
 </template>
@@ -38,6 +50,11 @@
 <script setup>
 import { ref } from 'vue'
 const loading = ref(false)
+
+const items = ref([
+  { text: 'Logout', icon: 'mdi-logout' },
+]);
+
 </script>
 
 <style></style>
