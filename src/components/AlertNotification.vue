928960bd14c8deac2b7ec0752afbabca10ffb667
Steto Javellana <javellanasteto99@gmail.com>
2025-01-15 23:27:25 +0800

    fix alert notif

--- src/components/AlertNotification.vue
+++ src/components/AlertNotification.vue
@@ -1,17 +1,17 @@
 <template>
-    <div>
-        <VScrollXReverseTransition>
-            <VAlert v-model="alert" :type="type" border="start" elevation="2" :border-color="type"
-                :icon="icon" prominent closable :text="text">
-            </VAlert>
-        </VScrollXReverseTransition>
-    </div>
+  <div>
+    <VScrollXReverseTransition>
+      <VAlert v-model="alert" :type="type" border="start" elevation="2" :border-color="props.type"
+        :icon="icon" prominent closable :text="props.text">
+      </VAlert>
+    </VScrollXReverseTransition>
+  </div>
 </template>
 
 <script setup>
 import { defineProps, ref } from 'vue'
 
-defineProps({
+const props = defineProps({
   text: {
     type: String,
     required: true
@@ -20,7 +20,7 @@ defineProps({
     type: String,
     required: true
   }
-})
+});
 
 const alert = ref(false);
 
@@ -30,23 +30,29 @@ const icon = computed(() => {
     error: 'mdi-alert',
     warning: 'mdi-alert',
     info: 'mdi-info'
-  }[type];
+  }[props.type];
 });
 
 watch(alert, (newVal) => {
-    if (newVal) {
-        setTimeout(() => { alert.value = false }, 3000)
-    }
+  if (newVal) {
+    setTimeout(() => { alert.value = false }, 3000)
+  }
 });
 
 </script>
 
 <style lang="scss" scoped>
 .v-alert {
-    position: fixed;
-    top: 15%;
-    right: 2%;
-    margin: 0 auto;
-    display: flex;
+  position: fixed;
+  top: 15%;
+  right: 2%;
+  margin: 0 auto;
+  display: flex;
+}
+</style>
+
+<style>
+.v-alert .v-alert__close {
+  align-self: center !important;
 }
 </style>
