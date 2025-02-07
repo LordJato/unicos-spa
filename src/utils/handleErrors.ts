355611701a833820c8113d780de1014a06660ed4
Steto Javellana <javellanasteto99@gmail.com>
2025-02-07 23:55:28 +0800

    handleErrors refactor to ts

---
+++ src/utils/handleErrors.ts
@@ -0,0 +1,26 @@
+import { useAlertNotificationStore } from "@/stores/alertNotification";
+import { unwrapErrorResponse } from "@/utils/api";
+
+interface ErrorResponse {
+  message: string;
+  errors?: Record<string, string[]>;
+}
+
+export default function handleErrors(
+  error: unknown,
+  errorMessages: Record<string, string | null>
+): void {
+  const alertStore = useAlertNotificationStore();
+  const errorResponse: ErrorResponse = unwrapErrorResponse(error);
+
+  alertStore.showAlert({
+    text: errorResponse.message,
+    type: "error",
+  });
+
+  if (errorResponse.errors && Object.keys(errorResponse.errors).length) {
+    Object.keys(errorMessages).forEach((key) => {
+      errorMessages[key] = errorResponse.errors?.[key]?.[0] || null;
+    });
+  }
+}
