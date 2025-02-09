f36876034ddea2faabe1a480866cac6c04da9b20
Steto Javellana <javellanasteto99@gmail.com>
2025-02-10 00:03:23 +0800

    refactor all to ts

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -1,25 +1,28 @@
 /**
- * plugins/index.js
+ * plugins/index.ts
  *
- * Automatically included in `./src/main.js`
+ * Automatically included in `./src/main.ts`
  */
 
 // Plugins
-import vuetify from './vuetify.ts'
-import router from '@/router'
-import { createPinia } from 'pinia';
-import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
-import { markRaw } from 'vue'
-import VueApexCharts from "vue3-apexcharts";
+import vuetify from './vuetify';
+import router from '@/router';
+import { createPinia, Pinia } from 'pinia';
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate';
+import { markRaw, App } from 'vue';
+import VueApexCharts from 'vue3-apexcharts';
 
-export function registerPlugins(app) {
-  const pinia = createPinia()
+export function registerPlugins(app: App): void {
+  const pinia: Pinia = createPinia();
+  
   pinia.use(({ store }) => {
-    store.router = markRaw(router)
-  })
-  pinia.use(piniaPluginPersistedstate)
-  app.use(router)
-  app.use(vuetify)
-  app.use(pinia)
+    (store as any).router = markRaw(router);
+  });
+
+  pinia.use(piniaPluginPersistedstate);
+
+  app.use(router);
+  app.use(vuetify);
+  app.use(pinia);
   app.use(VueApexCharts);
 }
