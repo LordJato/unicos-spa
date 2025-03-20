aaf37366f6d33ddc80bdf713150cca5d945a9432
Steto Javellana <javellanasteto99@gmail.com>
2025-03-20 23:16:20 +0800

    preparing designation, employee status and type

---
+++ src/views/hr/DesignationView.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        Designation
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/hr/EmployeeTypeView.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        Employee Type View
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
