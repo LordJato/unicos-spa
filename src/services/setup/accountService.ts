0011d4225523167ec87664d5aa183f83c315152f
Steto Javellana <javellanasteto99@gmail.com>
2025-03-23 23:30:10 +0800

    fix account service

---
+++ src/services/setup/accountService.ts
@@ -0,0 +1,62 @@
+import axios from "@/plugins/axios";
+import { unwrapSuccessResponse } from "@/utils/apiResponse";
+import type { Account } from "@/types/account";
+
+const API_URL = "accounts";
+
+export default {
+
+  // Fetch all account
+  async fetchAccounts(payload?: Record<string, any>) {
+    try {
+      const response = await axios.get(API_URL, { params: payload });
+      return unwrapSuccessResponse(response).data.records;
+    } catch (error) {
+      console.error("Error fetching accounts:", error);
+      throw error;
+    }
+  },
+
+  // Fetch a single account by ID
+  async fetchAccountById(id: number) {
+    try {
+      const response = await axios.get(`${API_URL}/${id}`);
+      return unwrapSuccessResponse(response).data;
+    } catch (error) {
+      console.error(`Error fetching account with ID ${id}:`, error);
+      throw error;
+    }
+  },
+
+  // Create a new account
+  async createAccount(payload: Omit<Account, "id">) {
+    try {
+      const response = await axios.post(API_URL, payload);
+      return unwrapSuccessResponse(response).data.records;
+    } catch (error) {
+      console.error("Error creating account:", error);
+      throw error;
+    }
+  },
+
+  // Update an existing account
+  async updateAccount(id: number, payload: Partial<Omit<Account, "id">>) {
+    try {
+      const response = await axios.put(`${API_URL}/${id}`, payload);
+      return unwrapSuccessResponse(response).data;
+    } catch (error) {
+      console.error(`Error updating account with ID ${id}:`, error);
+      throw error; 
+    }
+  },
+
+  // Delete an account
+  async deleteAccount(id: number) {
+    try {
+      await axios.delete(`${API_URL}/${id}`);
+    } catch (error) {
+      console.error(`Error deleting account with ID ${id}:`, error);
+      throw error;
+    }
+  },
+};
