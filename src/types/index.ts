55eb7f0669b8a785da7dee58a1e452e7548821bf
Steto Javellana <javellanasteto99@gmail.com>
2025-03-27 23:14:44 +0800

    add index on types

---
+++ src/types/index.ts
@@ -0,0 +1,6 @@
+export type { Account } from "./account";
+export type { Company } from "./company";
+export type { Department } from "./department";
+export type { CompanyEvent } from "./companyEvent";
+export type { Holiday } from "./holiday";
+export type { Designation } from "./designation";
