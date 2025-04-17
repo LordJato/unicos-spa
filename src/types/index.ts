0b67182379a0b79e570d5e1b467a2c58aa22cf16
Steto Javellana <javellanasteto99@gmail.com>
2025-04-17 22:47:54 +0800

    add opportunity types

--- src/types/index.ts
+++ src/types/index.ts
@@ -4,3 +4,4 @@ export type { Department } from "./department";
 export type { CompanyEvent } from "./companyEvent";
 export type { Holiday } from "./holiday";
 export type { Designation } from "./designation";
+export type { Opportunity } from "./opportunity";
