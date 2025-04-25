5855621b9507a49e55ba6eed3a07dec76ad18a4a
Steto Javellana <javellanasteto99@gmail.com>
2025-04-25 23:23:07 +0800

    fixed opportunity interface

--- src/types/opportunity.ts
+++ src/types/opportunity.ts
@@ -1,6 +1,17 @@
 export interface Opportunity {
-    id: number;
-    companyId: number;
-    name: string;
-  }
-  
+  id?: number;
+  company_id: number;
+  opportunity_type_id: number;
+  opportunity_status_id: number;
+  designation_id: number;
+  career_level_id: number;
+  title: string;
+  slug: string;
+  description: string;
+  location: string;
+  schedule: string;
+  years_of_experience: number;
+  vacancy: number;
+  created_at?: string;
+  updated_at?: string;
+};
