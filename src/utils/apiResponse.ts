807205a96df7124493116050417da55d53bd01fb
Steto Javellana <javellanasteto99@gmail.com>
2025-09-19 23:04:38 +0800

    Update apiResponse.ts

--- src/utils/apiResponse.ts
+++ src/utils/apiResponse.ts
@@ -4,6 +4,3 @@ export function unwrapSuccessResponse<T>(success: AxiosResponse<T>): T {
   return success.data;
 }
 
-export function unwrapErrorResponse<T = unknown>(error: AxiosError<T>): T {
-  return error.response?.data as T;
-}
