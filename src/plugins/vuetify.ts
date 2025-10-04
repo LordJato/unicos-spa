31c28f3ca05bf0a7624cf0947e0ffa212e954f61
Steto Javellana <javellanasteto99@gmail.com>
2025-10-04 23:36:20 +0800

    Update vuetify.ts

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -8,7 +8,6 @@
 import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/styles'
 
-// Composables
 import { createVuetify, type ThemeDefinition } from 'vuetify'
 
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
